91d6f42591d632d4f4e35a1a3a2d726918d59ffd
Hannah Smith <hannahsmith@Hannahs-MacBook-Pro.local>
2016-02-23 20:56:13 -0500

    wrote html header and main nav with some css

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# 4.2-Etsy-Recreation
+## 4 2 Etsy Recreation
---
+++ app/scripts/index.js
@@ -0,0 +1,5 @@
+var $ = require('jquery');
+var _ = require('underscore');
+var handlebars = require('handlebars');
+
+//console.log("Hello World!");
