6de534ed3e529464c7746ea0827c61817d6aa59a
Hannah Smith <hannahsmith@Hannahs-MacBook-Pro.local>
2016-02-24 21:40:38 -0500

    added side nav and did handlebars and jsonp

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -3,8 +3,53 @@
 var $ = require('jquery');
 var _ = require('underscore');
 var handlebars = require('handlebars');
+var context;
+// var numSearchResults = data.count;
+// var script = $('#search-text').html();
+// var templateScript = handlebars.compile(script);
+// var context = {
+//   'numOfResults': numSearchResults,
+//   'searchShop': 'terrarium'
+// }
+// var compiledHtml = templateScript(context);
+// ​
+// $('.search-text-section').html(compiledHtml);
 
-//console.log("Hello World!");
+//var dataSearchResults =
+var url = "https://api.etsy.com/v2/listings/active.js?api_key=f55b5cjaguj9rxkrf4krjpyc&keywords=tacos&includes=Images,Shop";
+
+function fetchJSONP(url, callback) {
+    var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+    var script = document.createElement('script');
+
+    window[callbackName] = function(data) {
+        delete window[callbackName];
+        document.body.removeChild(script);
+        callback(data);
+    };
+
+    script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+    document.body.appendChild(script);
+}
+var source = $("#show-template").html();
+var template = handlebars.compile(source);
+
+function logData(data){
+  var dataSearchResults = data.results;
+  console.log(dataSearchResults);
+   dataSearchResults.forEach(function(value, index, thisArray){
+     console.log('success');
+     var context = {
+      price: thisArray[index].price,
+      title: thisArray[index].title,
+      currency_code: thisArray[index].currency_code,
+      images: thisArray[index].Images[0].url_75x75,
+    }
+    $('.thumbnail').append(template(context));
+  })
+}
+
+fetchJSONP(url, logData)
 
 },{"handlebars":32,"jquery":44,"underscore":47}],2:[function(require,module,exports){
 (function (process,__filename){
